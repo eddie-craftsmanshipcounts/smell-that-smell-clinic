963cc2f6be507d735b7df00c912eccff11b11850
Eddie Bush <eddie@craftsmanshipcounts.com>
2021-10-17 19:46:17 -0500

    Improve names for TennisGame1

--- src/main/java/kata/tennis/TennisGame1.java
+++ src/main/java/kata/tennis/TennisGame1.java
@@ -3,30 +3,30 @@ package kata.tennis;
 public class TennisGame1 implements TennisGame
 {
 
-	private int m_score1 = 0;
-	private int m_score2 = 0;
+	private int player1Points = 0;
+	private int player2Points = 0;
 	private String player1Name;
 	private String player2Name;
 
-	public TennisGame1(String player1Name, String player2Name) {
+	public TennisGame1(String player1Name, String player2Name)
+	{
 		this.player1Name = player1Name;
 		this.player2Name = player2Name;
 	}
 
-	public void wonPoint(String playerName) {
+	public void wonPoint(String playerName)
+	{
 		if (playerName == "player1")
-			m_score1 += 1;
+			player1Points += 1;
 		else
-			m_score2 += 1;
+			player2Points += 1;
 	}
 
-	public String getScore() {
+	public String getScore()
+	{
 		String score = "";
-		int tempScore=0;
-		if (m_score1==m_score2)
-		{
-			switch (m_score1)
-			{
+		if (player1Points == player2Points) {
+			switch (player1Points) {
 				case 0:
 					score = "Love-All";
 					break;
@@ -41,34 +41,32 @@ public class TennisGame1 implements TennisGame
 					break;
 
 			}
-		}
-		else if (m_score1>=4 || m_score2>=4)
-		{
-			int minusResult = m_score1-m_score2;
-			if (minusResult==1) score ="Advantage player1";
-			else if (minusResult ==-1) score ="Advantage player2";
-			else if (minusResult>=2) score = "Win for player1";
-			else score ="Win for player2";
-		}
-		else
-		{
-			for (int i=1; i<3; i++)
-			{
-				if (i==1) tempScore = m_score1;
-				else { score+="-"; tempScore = m_score2;}
-				switch(tempScore)
-				{
+		} else if (player1Points >= 4 || player2Points >= 4) {
+			int scoreDifference = player1Points - player2Points;
+			if (scoreDifference == 1) score = "Advantage player1";
+			else if (scoreDifference == -1) score = "Advantage player2";
+			else if (scoreDifference >= 2) score = "Win for player1";
+			else score = "Win for player2";
+		} else {
+			int playerPoints = 0;
+			for (int player = 1; player < 3; player++) {
+				if (player == 1) playerPoints = player1Points;
+				else {
+					score += "-";
+					playerPoints = player2Points;
+				}
+				switch (playerPoints) {
 					case 0:
-						score+="Love";
+						score += "Love";
 						break;
 					case 1:
-						score+="Fifteen";
+						score += "Fifteen";
 						break;
 					case 2:
-						score+="Thirty";
+						score += "Thirty";
 						break;
 					case 3:
-						score+="Forty";
+						score += "Forty";
 						break;
 				}
 			}
